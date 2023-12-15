1becfe53d86fe7b589b365a3c704d88b2e756c12
Elias Carvalho <73039601+eliascarv@users.noreply.github.com>
2023-12-15 11:51:46 -0300

    Fix: accept subgeotable in `fitpredict` function (#6)
    * Fix: accept subgeotable in 'fitpredict' function
    
    * Add comment to test
    
    ---------
    
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- test/utils.jl
+++ test/utils.jl
@@ -6,6 +6,11 @@
   @test pred.b == gtb.b
   @test pred.geometry == gtb.geometry
 
+  # also works with views
+  vgtb = view(gtb, 1:3)
+  vpred = GeoStatsModels.fitpredict(IDW(), vgtb, pset, neighbors=false)
+  @test vpred == pred
+
   gtb = georef((; z=[1.0, 0.0, 1.0]), [(25.0, 25.0), (50.0, 75.0), (75.0, 50.0)])
   grid = CartesianGrid((100, 100), (0.5, 0.5), (1.0, 1.0))
   linds = LinearIndices(size(grid))
