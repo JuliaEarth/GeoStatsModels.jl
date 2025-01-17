510a3ca946f06564be1a668ff22b5a2a01790c78
Júlio Hoffimann <julio.hoffimann@gmail.com>
2025-01-17 13:59:52 -0300

    Optimize ordinary.jl

--- src/krig/ordinary.jl
+++ src/krig/ordinary.jl
@@ -17,22 +17,24 @@ function lhsconstraints!(model::OrdinaryKriging, LHS::AbstractMatrix, nvar::Int,
   # number of constraints
   ncon = nconstraints(model, nvar)
 
-  # index of first constraint
-  ind = size(LHS, 1) - ncon + 1
+  # retrieve size of LHS
+  nrow, ncol = size(LHS)
 
-  # auxiliary variables
-  Iₖ = I(nvar)
+  # index of first constraint
+  ind = nrow - ncon + 1
 
   # set identity blocks
-  @inbounds for j in 1:nvar:(ind - nvar)
-    LHS[ind:end, j:(j + nvar - 1)] .= Iₖ
+  @inbounds for j in 1:(ind - 1), i in ind:nrow
+    LHS[i, j] = (i == mod1(j, nvar) + ind - 1)
   end
-  @inbounds for i in 1:nvar:(ind - nvar)
-    LHS[i:(i + nvar - 1), ind:end] .= Iₖ
+  @inbounds for j in ind:ncol, i in 1:(ind - 1)
+    LHS[i, j] = LHS[j, i]
   end
 
   # set zero block
-  @inbounds LHS[ind:end, ind:end] .= zero(eltype(LHS))
+  @inbounds for j in ind:ncol, i in ind:nrow
+    LHS[i, j] = 0
+  end
 
   nothing
 end
