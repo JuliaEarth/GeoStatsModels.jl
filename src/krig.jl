7d8d33ff86c1f38542e4ee76071903ad13e4236d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-07-07 11:33:25 -0300

    More refactoring

--- src/krig.jl
+++ src/krig.jl
@@ -12,16 +12,15 @@ abstract type KrigingModel <: GeoStatsModel end
 Base.range(model::KrigingModel) = range(model.fun)
 
 """
-    KrigingState(data, LHS, RHS, FHS, nfun, miss)
+    KrigingState(data, FHS, RHS, nfun, miss)
 
 A Kriging state stores information needed
 to perform estimation at any given geometry.
 """
-mutable struct KrigingState{D<:AbstractGeoTable,L,R,F}
+mutable struct KrigingState{D<:AbstractGeoTable,F,R}
   data::D
-  LHS::L
-  RHS::R
   FHS::F
+  RHS::R
   nfun::Int
   miss::Vector{Int}
 end
@@ -67,10 +66,10 @@ function fit(model::KrigingModel, data)
   nfun, miss = setlhs!(model, LHS, data)
 
   # factorize LHS
-  FHS = lhsfactorize(model, LHS, miss)
+  FHS = lhsfactorize!(model, LHS, miss)
 
   # record Kriging state
-  state = KrigingState(data, LHS, RHS, FHS, nfun, miss)
+  state = KrigingState(data, FHS, RHS, nfun, miss)
 
   FittedKriging(model, state)
 end
@@ -139,14 +138,14 @@ function setlhs!(model::KrigingModel, LHS, data)
 end
 
 # choose appropriate factorization of LHS
-lhsfactorize(model::GeoStatsModel, LHS, miss) = _lhsfactorize(model.fun, LHS, miss)
+lhsfactorize!(model::GeoStatsModel, LHS, miss) = _lhsfactorize!(model.fun, LHS, miss)
 
 # enforce SVD factorization for rank-deficient matrices
 # use QRIteration to avoid LAPACK bug: https://github.com/Reference-LAPACK/lapack/issues/672
-_lhsfactorize(::Transiogram, LHS, miss) = svd!(LHS, alg=QRIteration())
+_lhsfactorize!(::Transiogram, LHS, miss) = svd!(LHS, alg=QRIteration())
 
 # choose appropriate factorization for other functions (e.g., CompositeFunction)
-function _lhsfactorize(fun::GeoStatsFunction, LHS, miss)
+function _lhsfactorize!(fun::GeoStatsFunction, LHS, miss)
   if isempty(miss)
     if issymmetric(fun)
       # enforce Bunch-Kaufman factorization
