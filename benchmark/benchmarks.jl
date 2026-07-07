d13f3058ee63894a1314c382d07d999420f23bd5
Júlio Hoffimann <julio.hoffimann@gmail.com>
2026-07-07 10:04:22 -0300

    Add benchmarks (#48)
    * Choose different factorization in the presence of missing data
    
    * Undo changes in krig.jl

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,25 @@
+using BenchmarkTools
+using GeoStatsModels
+using GeoTables
+using Meshes
+
+# auxiliary variables
+table1 = (; z=[1.0, 2.0, 3.0])
+table2 = (; z=[missing, 2.0, 3.0])
+coords = [(25.0, 25.0), (50.0, 75.0), (75.0, 50.0)]
+data1 = georef(table1, coords)
+data2 = georef(table2, coords)
+igrid = CartesianGrid(100, 100)
+model = Kriging(GaussianVariogram(range=35.0))
+
+# initialize benchmark suite
+const SUITE = BenchmarkGroup()
+
+# --------
+# KRIGING
+# --------
+
+SUITE["kriging"] = BenchmarkGroup()
+
+SUITE["kriging"]["full"] = @benchmarkable GeoStatsModels.fitpredict($model, $data1, $igrid, neighbors=true)
+SUITE["kriging"]["miss"] = @benchmarkable GeoStatsModels.fitpredict($model, $data2, $igrid, neighbors=true)
